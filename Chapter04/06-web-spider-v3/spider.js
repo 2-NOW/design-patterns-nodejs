e2c7230de4a16a7f97828154bbd1375053db9f04
2-NOW <kj109888@gmail.com>
2021-09-20 22:59:36 +0900

    04_06

--- Chapter04/06-web-spider-v3/spider.js
+++ Chapter04/06-web-spider-v3/spider.js
@@ -1,77 +1,104 @@
-import fs from 'fs'
-import path from 'path'
-import superagent from 'superagent'
-import mkdirp from 'mkdirp'
-import { urlToFilename, getPageLinks } from './utils.js'
+import fs from 'fs';
+import path from 'path';
+import superagent from 'superagent';
+import mkdirp from 'mkdirp';
+import { urlToFilename, getPageLinks } from './utils.js';
 
-function saveFile (filename, contents, cb) {
-  mkdirp(path.dirname(filename), err => {
+function saveFile(filename, contents, cb) {
+  mkdirp(path.dirname(filename), (err) => {
     if (err) {
-      return cb(err)
+      return cb(err);
     }
-    fs.writeFile(filename, contents, cb)
-  })
+    fs.writeFile(filename, contents, cb);
+  });
 }
 
-function download (url, filename, cb) {
-  console.log(`Downloading ${url}`)
+function download(url, filename, cb) {
+  console.log(`Downloading ${url}`);
   superagent.get(url).end((err, res) => {
     if (err) {
-      return cb(err)
+      return cb(err);
     }
-    saveFile(filename, res.text, err => {
+    saveFile(filename, res.text, (err) => {
+      if (err) {
+        return cb(err);
+      }
+      console.log(`Downloaded and saved: ${url}`);
+      cb(null, res.text);
+    });
+  });
+}
+
+function spiderLinks(currentUrl, body, nesting, cb) {
+  if (nesting === 0) {
+    return process.nextTick(cb);
+  }
+
+  const links = getPageLinks(currentUrl, body);
+  if (links.length === 0) {
+    return process.nextTick(cb);
+  }
+  /*
+
+  function iterate (index) { // [2]
+    if (index === links.length) {
+      return cb()
+    }
+
+    spider(links[index], nesting - 1, function (err) { // [3]
       if (err) {
         return cb(err)
       }
-      console.log(`Downloaded and saved: ${url}`)
-      cb(null, res.text)
+      iterate(index + 1)
     })
-  })
+  }
+
+  iterate(0) // [4]
 }
 
-function spiderLinks (currentUrl, body, nesting, cb) {
-  if (nesting === 0) {
-    return process.nextTick(cb)
-  }
+원래 있던 부분 - iterate 재귀로 spider 호출을 했었다.
+*/
 
-  const links = getPageLinks(currentUrl, body)
-  if (links.length === 0) {
-    return process.nextTick(cb)
-  }
+  // 여기서부터 v2랑 달라짐
 
-  let completed = 0
-  let hasErrors = false
+  let completed = 0;
+  let hasErrors = false;
 
-  function done (err) {
+  function done(err) {
     if (err) {
-      hasErrors = true
-      return cb(err)
+      hasErrors = true;
+      return cb(err);
     }
     if (++completed === links.length && !hasErrors) {
-      return cb()
+      return cb();
     }
   }
 
-  links.forEach(link => spider(link, nesting - 1, done))
+  links.forEach((link) => spider(link, nesting - 1, done));
 }
 
-export function spider (url, nesting, cb) {
-  const filename = urlToFilename(url)
+// 여기까지
+// links배열에 담긴 link 각각에 대해 차례로 spider를 부름
+// readFile은 비동기 함수이므로 병렬(처럼 보이게) 실행을 한다.
+// 각각의 작업에 대해 완료되면 completed를 받아서 complete와 배열의 길이를 비교
+
+export function spider(url, nesting, cb) {
+  const filename = urlToFilename(url);
   fs.readFile(filename, 'utf8', (err, fileContent) => {
     if (err) {
       if (err.code !== 'ENOENT') {
-        return cb(err)
+        return cb(err);
       }
 
       return download(url, filename, (err, requestContent) => {
         if (err) {
-          return cb(err)
+          return cb(err);
         }
 
-        spiderLinks(url, requestContent, nesting, cb)
-      })
+        spiderLinks(url, requestContent, nesting, cb);
+      });
     }
 
-    spiderLinks(url, fileContent, nesting, cb)
-  })
+    spiderLinks(url, fileContent, nesting, cb);
+  });
 }
