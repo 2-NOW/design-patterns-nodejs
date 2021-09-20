fc6dae1c42a93d79d59dcd675e7b517bd3520f84
2-NOW <kj109888@gmail.com>
2021-09-20 22:27:57 +0900

    IIFE

--- Chapter03/01-sync-cps/index.js
+++ Chapter03/01-sync-cps/index.js
@@ -6,6 +6,10 @@ function addCps(a, b, callback) {
 
 // 즉시실행함수
 
+function addCps(a, b, callback) {
+  callback(a + b);
+}
+
 console.log('before');
 addCps(1, 2, (result1, result2) =>
   console.log(`Result1 : ${result1}, ${result2}}`)
