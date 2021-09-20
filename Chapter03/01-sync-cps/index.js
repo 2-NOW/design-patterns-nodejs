e3398a2d5aa0662d0ff6cee7dfe651620e2d1f0b
2-NOW <kj109888@gmail.com>
2021-09-20 15:28:02 +0900

    01

--- Chapter03/01-sync-cps/index.js
+++ Chapter03/01-sync-cps/index.js
@@ -1,9 +1,15 @@
 /* eslint standard/no-callback-literal: 0 */
 
-function addCps (a, b, callback) {
-  callback(a + b)
+function addCps(a, b, callback) {
+  callback(
+    (function (a, b) {
+      return a + b;
+    })(a, b)
+  );
 }
 
-console.log('before')
-addCps(1, 2, result => console.log(`Result: ${result}`))
-console.log('after')
+console.log('before');
+addCps(1, 2, (result1, result2) =>
+  console.log(`Result1 : ${result1}, ${result2}}`)
+);
+console.log('after');
