89a2585590aa77851b61e39b99718db3c79bfffa
2-NOW <kj109888@gmail.com>
2021-09-22 13:33:39 +0900

    04_11

--- Chapter04/11-web-spider-v4/spider.js
+++ Chapter04/11-web-spider-v4/spider.js
@@ -1,78 +1,84 @@
-import fs from 'fs'
-import path from 'path'
-import superagent from 'superagent'
-import mkdirp from 'mkdirp'
-import { urlToFilename, getPageLinks } from './utils.js'
+import fs from 'fs';
+import path from 'path';
+import superagent from 'superagent';
+import mkdirp from 'mkdirp';
+import { urlToFilename, getPageLinks } from './utils.js';
 
-function saveFile (filename, contents, cb) {
-  mkdirp(path.dirname(filename), err => {
+function saveFile(filename, contents, cb) {
+  mkdirp(path.dirname(filename), (err) => {
     if (err) {
-      return cb(err)
+      return cb(err);
     }
-    fs.writeFile(filename, contents, cb)
-  })
+    fs.writeFile(filename, contents, cb);
+  });
 }
 
-function download (url, filename, cb) {
-  console.log(`Downloading ${url}`)
+// save 함수
+
+function download(url, filename, cb) {
+  console.log(`Downloading ${url}`);
   superagent.get(url).end((err, res) => {
     if (err) {
-      return cb(err)
+      return cb(err);
     }
-    saveFile(filename, res.text, err => {
+    saveFile(filename, res.text, (err) => {
       if (err) {
-        return cb(err)
+        return cb(err);
       }
-      console.log(`Downloaded and saved: ${url}`)
-      cb(null, res.text)
-    })
-  })
+      console.log(`Downloaded and saved: ${url}`);
+      cb(null, res.text);
+    });
+  });
 }
 
-function spiderLinks (currentUrl, body, nesting, queue) {
+// 다운로드 함수
+
+function spiderLinks(currentUrl, body, nesting, queue) {
   if (nesting === 0) {
-    return
+    return;
   }
 
-  const links = getPageLinks(currentUrl, body)
+  const links = getPageLinks(currentUrl, body);
   if (links.length === 0) {
-    return
+    return;
   }
 
-  links.forEach(link => spider(link, nesting - 1, queue))
+  links.forEach((link) => spider(link, nesting - 1, queue));
 }
 
-function spiderTask (url, nesting, queue, cb) {
-  const filename = urlToFilename(url)
+// Link 가져오는 함수
+
+function spiderTask(url, nesting, queue, cb) {
+  const filename = urlToFilename(url);
   fs.readFile(filename, 'utf8', (err, fileContent) => {
     if (err) {
       if (err.code !== 'ENOENT') {
-        return cb(err)
+        return cb(err);
       }
 
       return download(url, filename, (err, requestContent) => {
         if (err) {
-          return cb(err)
+          return cb(err);
         }
 
-        spiderLinks(url, requestContent, nesting, queue)
-        return cb()
-      })
+        spiderLinks(url, requestContent, nesting, queue);
+        return cb();
+      });
     }
 
-    spiderLinks(url, fileContent, nesting, queue)
-    return cb()
-  })
+    spiderLinks(url, fileContent, nesting, queue);
+    return cb();
+  });
 }
 
-const spidering = new Set()
-export function spider (url, nesting, queue) {
+const spidering = new Set();
+export function spider(url, nesting, queue) {
   if (spidering.has(url)) {
-    return
+    return;
   }
 
-  spidering.add(url)
+  spidering.add(url);
   queue.pushTask((done) => {
-    spiderTask(url, nesting, queue, done)
-  })
+    spiderTask(url, nesting, queue, done);
+  });
 }
