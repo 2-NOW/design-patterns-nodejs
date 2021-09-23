0d5b6b869679b5b7d6a4dd7af4aaedcc7fb8d5ca
2-NOW <kj109888@gmail.com>
2021-09-23 22:51:06 +0900

    chapter07

--- Chapter05/01-delay-with-promises/index.js
+++ Chapter05/01-delay-with-promises/index.js
@@ -1,14 +1,16 @@
-function delay (milliseconds) {
+function delay(milliseconds) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(new Date())
-    }, milliseconds)
-  })
+      resolve(new Date());
+    }, milliseconds);
+  });
 }
 
-console.log(`Delaying...${new Date().getSeconds()}s`)
+console.log(`Delaying...${new Date().getSeconds()}s`);
 
-delay(1000)
-  .then(newDate => {
-    console.log(`Done ${newDate.getSeconds()}s`)
-  })
+delay(1000).then((newDate) => {
+  console.log(`Done ${newDate.getSeconds()}s`);
+});
+
+// resolve는 fulfillment value를 받는 값
+// 성공하면 뭐 한다 ~ 라는 뜻
--- Chapter05/05-promises-web-spider-v4/spider.js
+++ Chapter05/05-promises-web-spider-v4/spider.js
@@ -1,64 +1,64 @@
-import { promises as fsPromises } from 'fs'
-import { dirname } from 'path'
-import superagent from 'superagent'
-import mkdirp from 'mkdirp'
-import { urlToFilename, getPageLinks } from './utils.js'
-import { promisify } from 'util'
-import { TaskQueue } from './TaskQueue.js'
+import { promises as fsPromises } from 'fs';
+import { dirname } from 'path';
+import superagent from 'superagent';
+import mkdirp from 'mkdirp';
+import { urlToFilename, getPageLinks } from './utils.js';
+import { promisify } from 'util';
+import { TaskQueue } from './TaskQueue.js';
 
-const mkdirpPromises = promisify(mkdirp)
+const mkdirpPromises = promisify(mkdirp);
 
-function download (url, filename) {
-  console.log(`Downloading ${url}`)
-  let content
-  return superagent.get(url)
-    .then(res => {
-      content = res.text
-      return mkdirpPromises(dirname(filename))
+function download(url, filename) {
+  console.log(`Downloading ${url}`);
+  let content;
+  return superagent
+    .get(url)
+    .then((res) => {
+      content = res.text;
+      return mkdirpPromises(dirname(filename));
     })
     .then(() => fsPromises.writeFile(filename, content))
     .then(() => {
-      console.log(`Downloaded and saved: ${url}`)
-      return content
-    })
+      console.log(`Downloaded and saved: ${url}`);
+      return content;
+    });
 }
 
-function spiderLinks (currentUrl, content, nesting, queue) {
+function spiderLinks(currentUrl, content, nesting, queue) {
   if (nesting === 0) {
-    return Promise.resolve()
+    return Promise.resolve();
   }
 
-  const links = getPageLinks(currentUrl, content)
-  const promises = links.map(link => spiderTask(link, nesting - 1, queue))
+  const links = getPageLinks(currentUrl, content);
+  const promises = links.map((link) => spiderTask(link, nesting - 1, queue));
 
-  return Promise.all(promises)
+  return Promise.all(promises);
 }
 
-const spidering = new Set()
-function spiderTask (url, nesting, queue) {
+const spidering = new Set();
+function spiderTask(url, nesting, queue) {
   if (spidering.has(url)) {
-    return Promise.resolve()
+    return Promise.resolve();
   }
-  spidering.add(url)
+  spidering.add(url);
 
-  const filename = urlToFilename(url)
+  const filename = urlToFilename(url);
 
   return queue
     .runTask(() => {
-      return fsPromises.readFile(filename, 'utf8')
-        .catch((err) => {
-          if (err.code !== 'ENOENT') {
-            throw err
-          }
+      return fsPromises.readFile(filename, 'utf8').catch((err) => {
+        if (err.code !== 'ENOENT') {
+          throw err;
+        }
 
-          // The file doesn't exist, so let’s download it
-          return download(url, filename)
-        })
+        // The file doesn't exist, so let’s download it
+        return download(url, filename);
+      });
     })
-    .then(content => spiderLinks(url, content, nesting, queue))
+    .then((content) => spiderLinks(url, content, nesting, queue));
 }
 
-export function spider (url, nesting, concurrency) {
-  const queue = new TaskQueue(concurrency)
-  return spiderTask(url, nesting, queue)
+export function spider(url, nesting, concurrency) {
+  const queue = new TaskQueue(concurrency);
+  return spiderTask(url, nesting, queue);
 }
